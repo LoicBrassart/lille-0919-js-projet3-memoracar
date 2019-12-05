586e411c390ff6237b63e11ad25f31fbe5a9fd0e
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-12-05 17:53:08 +0100

    save

--- frontend/src/components/IconList.jsx
+++ frontend/src/components/IconList.jsx
@@ -19,23 +19,26 @@ class IconList extends React.Component {
       <div className="intFamilies">
         <div className="icones">
           <div className="module" id="moteur">
-            {this.state.Show ? (
-              <p>Vidange, filtre à huile, filtre à gasoil Echéance: 70 000km</p>
-            ) : null}
             <div id="imgIcone">
               <button onClick={() => this.notif()}>Moteur</button>
             </div>
+            {this.state.Show ? (
+              <p>Vidange, filtre à huile, filtre à gasoil Echéance: 70 000km</p>
+            ) : null}
           </div>
+
           <div className="module" id="freins">
             <div id="imgIcone">
               <h1>Freins</h1>
             </div>
           </div>
+
           <div className="module" id="chassis">
             <div id="imgIcone">
               <h1>Chassis</h1>
             </div>
           </div>
+
           <div className="module" id="pneus">
             <div id="imgIcone">
               <h1>Pneus</h1>
@@ -47,11 +50,13 @@ class IconList extends React.Component {
               <h1>Carrosserie</h1>
             </div>
           </div>
+
           <div className="module" id="electricite">
             <div id="imgIcone">
               <h1>Electricite</h1>
             </div>
           </div>
+
           <div className="module" id="echeances">
             <div id="imgIcone">
               <h1>Echeances</h1>
