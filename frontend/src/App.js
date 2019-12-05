765fda37420ab6e6391bf5245a1f69bab34f04cd
LaurentMahieu <mahieu.l@hotmail.fr>
2019-12-05 11:33:41 +0100

    create route to Login and to Signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Identification from "./pages/Identification";
+import Login from "./components/Login";
+import Signin from "./components/Signin";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Identification />
+      <Switch>
+        <Route path="/identification" component={Identification} />
+        <Route path="/login" component={Login} />
+        <Route path="/signin" component={Signin} />
+      </Switch>
       <HomePage />
       <NavBar />
     </div>
