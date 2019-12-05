acace18a1b3920df7ce6abfd230ee40ce68c06e5
david derancourt <david.derancourt@hotmail.fr>
2019-12-05 14:57:20 +0100

    creation of component HistoricCard

--- frontend/src/components/Historic.jsx
+++ frontend/src/components/Historic.jsx
@@ -1,37 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./style/Historic.scss";
+import HistoricCard from "./HistoricCard";
 
 function Historic(props) {
   return (
     <div className="HistoricBox">
-      <div className="ScrollBar">scrollbar</div>
       <div>
         <div className="ToCome">
           {props.ToCome.map(item => {
-            return (
-              <div className="EventBox BoxEvent">
-                <img src={item.icon} alt={item.title} />
-                <div className="InfoBox">
-                  <p>{item.title}</p>
-                  <p>done at {item.date}</p>
-                </div>
-              </div>
-            );
+            return <HistoricCard item={item} />;
           })}
         </div>
         <div className="Present BoxEvent">{Date()}</div>
         <div className="Passed">
-          {props.Passed.map(item => {
-            return (
-              <div className="EventBox BoxEvent">
-                <img src={item.icon} alt={item.title} />
-                <div className="InfoBox">
-                  <p>{item.title}</p>
-                  <p>done at {item.date}</p>
-                </div>
-              </div>
-            );
+          {props.ToCome.map(item => {
+            return <HistoricCard item={item} />;
           })}
         </div>
       </div>
