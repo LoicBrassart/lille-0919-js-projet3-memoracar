68959a0e04800ccdd3e0748af4bdbcd37a717c63
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-12-05 12:34:55 +0100

    icons position and color

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ListCar from "./ListeCar";
 import NavBar from "./NavBar";
 import Historic from "./Historic";
+import IconList from "./IconList";
 
 function HomePage() {
   return (
     <div>
       <ListCar />
+      <IconList />
       <Historic />
       <NavBar />
     </div>
---
+++ frontend/src/components/IconList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style/IconList.scss";
+
+function IconList() {
+  return (
+    <div>
+      <div class="grid">
+        <div class="module">
+          <h1>MOTEUR</h1>
+        </div>
+        <div class="module">FREINS</div>
+        <div class="module">CHASSIS</div>
+        <div class="module">PNEUS</div>
+
+        <div class="module">CAROSSERIE</div>
+        <div class="module">ELECTRICITE</div>
+        <div class="module">ECHEANCES</div>
+      </div>
+    </div>
+  );
+}
+
+export default IconList;
