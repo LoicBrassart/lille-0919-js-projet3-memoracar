27008e4065c975e260970945c225300c487c22be
david derancourt <david.derancourt@hotmail.fr>
2019-12-05 15:11:36 +0100

    creation of a button to switch between historic and listCar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ListCar from "./ListeCar";
 import Historic from "./Historic";
 
 function HomePage() {
+  const [toggle, setToggle] = useState(true);
+
   return (
     <div>
-      <ListCar />
-      <Historic />
+      <button
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        {toggle ? "historic" : "ListCar"}
+      </button>
+      {toggle ? <ListCar /> : <Historic />}
     </div>
   );
 }
