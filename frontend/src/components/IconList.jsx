22726c2d7941a5dee0c989510997dc966dca2f92
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-12-05 17:08:03 +0100

    button and notif show

--- frontend/src/components/IconList.jsx
+++ frontend/src/components/IconList.jsx
@@ -1,35 +1,66 @@
 import React from "react";
 import "./style/IconList.scss";
 
-function IconList() {
-  return (
-    <div className="intFamilies">
-      <div className="icones">
-        <div className="module" id="moteur">
-          <h1>Moteur</h1>
-        </div>
-        <div className="module" id="freins">
-          <h1>Freins</h1>
-        </div>
-        <div className="module" id="chassis">
-          <h1>Chassis</h1>
-        </div>
-        <div className="module" id="pneus">
-          <h1>Pneus</h1>
-        </div>
+class IconList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Show: false
+    };
+  }
+  notif() {
+    this.setState({
+      Show: !this.state.Show
+    });
+  }
 
-        <div className="module" id="carrosserie">
-          <h1>Carrosserie</h1>
-        </div>
-        <div className="module" id="electricité">
-          <h1>Electricite</h1>
-        </div>
-        <div className="module" id="echeances">
-          <h1>Echeances</h1>
+  render() {
+    return (
+      <div className="intFamilies">
+        <div className="icones">
+          <div className="module" id="moteur">
+            {this.state.Show ? (
+              <p>Vidange, filtre à huile, filtre à gasoil Echéance: 70 000km</p>
+            ) : null}
+            <div id="imgIcone">
+              <button onClick={() => this.notif()}>Moteur</button>
+            </div>
+          </div>
+          <div className="module" id="freins">
+            <div id="imgIcone">
+              <h1>Freins</h1>
+            </div>
+          </div>
+          <div className="module" id="chassis">
+            <div id="imgIcone">
+              <h1>Chassis</h1>
+            </div>
+          </div>
+          <div className="module" id="pneus">
+            <div id="imgIcone">
+              <h1>Pneus</h1>
+            </div>
+          </div>
+
+          <div className="module" id="carrosserie">
+            <div id="imgIcone">
+              <h1>Carrosserie</h1>
+            </div>
+          </div>
+          <div className="module" id="electricite">
+            <div id="imgIcone">
+              <h1>Electricite</h1>
+            </div>
+          </div>
+          <div className="module" id="echeances">
+            <div id="imgIcone">
+              <h1>Echeances</h1>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default IconList;
