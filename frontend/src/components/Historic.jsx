96ec5cf7984432efccbd6f9b6c29b5e5d1252ea7
david derancourt <david.derancourt@hotmail.fr>
2019-12-05 11:17:28 +0100

    map in the historic component. The component can display main informations from an array, no style

--- frontend/src/components/Historic.jsx
+++ frontend/src/components/Historic.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { connect } from "react-redux";
+import "./style/Historic.scss";
 
-function Historic() {
-  return <div>Historic</div>;
+function Historic(props) {
+  return (
+    <div className="HistoricBox">
+      <div className="ToCome">
+        {props.ToCome.map(item => {
+          return (
+            <div className="EventBox">
+              <p>{item.title}</p>
+              <p>done at {item.date}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="Present">date</div>
+      <div className="Passed">
+        {props.Passed.map(item => {
+          return (
+            <div className="EventBox">
+              <p>{item.title}</p>
+              <p>done at {item.date}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Historic;
+const mapStateToProps = state => {
+  return {
+    Passed: state.Passed,
+    ToCome: state.ToCome
+  };
+};
+
+export default connect(mapStateToProps)(Historic);
