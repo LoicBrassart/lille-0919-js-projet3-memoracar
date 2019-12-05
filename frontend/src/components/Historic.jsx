492a2689c071e12f880281aab7d1d6f052b07a67
david derancourt <david.derancourt@hotmail.fr>
2019-12-05 12:41:16 +0100

    creation of a scrollbar

--- frontend/src/components/Historic.jsx
+++ frontend/src/components/Historic.jsx
@@ -5,33 +5,35 @@ import "./style/Historic.scss";
 function Historic(props) {
   return (
     <div className="HistoricBox">
-      <div className="scrollBar"></div>
-      <div className="ToCome">
-        {props.ToCome.map(item => {
-          return (
-            <div className="EventBox">
-              <img src={item.icon} alt={item.title} />
-              <div className="InfoBox">
-                <p>{item.title}</p>
-                <p>done at {item.date}</p>
+      <div className="ScrollBar">scrollbar</div>
+      <div>
+        <div className="ToCome">
+          {props.ToCome.map(item => {
+            return (
+              <div className="EventBox BoxEvent">
+                <img src={item.icon} alt={item.title} />
+                <div className="InfoBox">
+                  <p>{item.title}</p>
+                  <p>done at {item.date}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="Present">{Date()}</div>
-      <div className="Passed">
-        {props.Passed.map(item => {
-          return (
-            <div className="EventBox">
-              <img src={item.icon} alt={item.title} />
-              <div className="InfoBox">
-                <p>{item.title}</p>
-                <p>done at {item.date}</p>
+            );
+          })}
+        </div>
+        <div className="Present BoxEvent">{Date()}</div>
+        <div className="Passed">
+          {props.Passed.map(item => {
+            return (
+              <div className="EventBox BoxEvent">
+                <img src={item.icon} alt={item.title} />
+                <div className="InfoBox">
+                  <p>{item.title}</p>
+                  <p>done at {item.date}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
