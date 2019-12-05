62c45e05dfb8a5312294157305d859701975c54a
LaurentMahieu <mahieu.l@hotmail.fr>
2019-12-05 11:01:41 +0100

    import Identification in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Identification from "./pages/Identification";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Identification />
       <HomePage />
       <NavBar />
     </div>
