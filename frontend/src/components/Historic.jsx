47b0a6c01d9c93ee2d245aad2c5dcb1fce3e9958
david derancourt <david.derancourt@hotmail.fr>
2019-12-05 12:40:03 +0100

    add a bit of style on historic page

--- frontend/src/components/Historic.jsx
+++ frontend/src/components/Historic.jsx
@@ -5,23 +5,30 @@ import "./style/Historic.scss";
 function Historic(props) {
   return (
     <div className="HistoricBox">
+      <div className="scrollBar"></div>
       <div className="ToCome">
         {props.ToCome.map(item => {
           return (
             <div className="EventBox">
-              <p>{item.title}</p>
-              <p>done at {item.date}</p>
+              <img src={item.icon} alt={item.title} />
+              <div className="InfoBox">
+                <p>{item.title}</p>
+                <p>done at {item.date}</p>
+              </div>
             </div>
           );
         })}
       </div>
-      <div className="Present">date</div>
+      <div className="Present">{Date()}</div>
       <div className="Passed">
         {props.Passed.map(item => {
           return (
             <div className="EventBox">
-              <p>{item.title}</p>
-              <p>done at {item.date}</p>
+              <img src={item.icon} alt={item.title} />
+              <div className="InfoBox">
+                <p>{item.title}</p>
+                <p>done at {item.date}</p>
+              </div>
             </div>
           );
         })}
