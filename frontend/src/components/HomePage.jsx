6e208d82ed7bdf6d04a8ebc6fc95ab3ed86e962c
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-12-05 18:05:00 +0100

    name change for  ListeCar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ListCar from "./ListeCar";
+import ListeCar from "./ListeCar";
 
 import Historic from "./Historic";
-import IconList from "./IconList";
 
 function HomePage() {
   return (
     <div>
-      <ListCar />
-      <IconList />
+      <ListeCar />
+
       <Historic />
     </div>
   );
--- frontend/src/components/ListeCar.jsx
+++ frontend/src/components/ListeCar.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import "./style/ListeCar.scss";
 
-function ListCar() {
-  return <div>ListCar</div>;
+class ListeCar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Show: false
+    };
+  }
+  notif() {
+    this.setState({
+      Show: !this.state.Show
+    });
+  }
+
+  render() {
+    return (
+      <div className="intFamilies">
+        <div className="icones">
+          <div className="module" id="moteur">
+            <div id="imgIcone">
+              <button onClick={() => this.notif()}>Moteur</button>
+            </div>
+            {this.state.Show ? (
+              <p>Vidange, filtre à huile, filtre à gasoil Echéance: 70 000km</p>
+            ) : null}
+          </div>
+
+          <div className="module" id="freins">
+            <div id="imgIcone">
+              <h1>Freins</h1>
+            </div>
+          </div>
+
+          <div className="module" id="chassis">
+            <div id="imgIcone">
+              <h1>Chassis</h1>
+            </div>
+          </div>
+
+          <div className="module" id="pneus">
+            <div id="imgIcone">
+              <h1>Pneus</h1>
+            </div>
+          </div>
+
+          <div className="module" id="carrosserie">
+            <div id="imgIcone">
+              <h1>Carrosserie</h1>
+            </div>
+          </div>
+
+          <div className="module" id="electricite">
+            <div id="imgIcone">
+              <h1>Electricite</h1>
+            </div>
+          </div>
+
+          <div className="module" id="echeances">
+            <div id="imgIcone">
+              <h1>Echeances</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ListCar;
+export default ListeCar;
