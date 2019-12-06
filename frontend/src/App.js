dd5373de5df0b363898bb6f144a86fbb44b87f04
Blandine <blandine.mallart@gmail.com>
2019-12-06 10:48:02 +0100

    modif PR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
-import "./reset.css"
+
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./reducer/reducer";
-
+import "./reset.css";
 
 
 const store = createStore(reducer);
