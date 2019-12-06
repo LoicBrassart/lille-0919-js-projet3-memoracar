90e272baac10ff4b03918b5d5293323ed499603a
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-12-06 09:27:03 +0100

    class to function

--- frontend/src/components/ListeCar.jsx
+++ frontend/src/components/ListeCar.jsx
@@ -1,71 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/ListeCar.scss";
 
-class ListeCar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      Show: false
-    };
-  }
-  notif() {
-    this.setState({
-      Show: !this.state.Show
-    });
-  }
+function ListeCar() {
+  const [ok, setOk] = useState("Tout est parfait !");
 
-  render() {
-    return (
-      <div className="intFamilies">
-        <div className="icones">
-          <div className="module" id="moteur">
-            <div id="imgIcone">
-              <button onClick={() => this.notif()}>Moteur</button>
-            </div>
-            {this.state.Show ? (
-              <p>Vidange, filtre à huile, filtre à gasoil Echéance: 70 000km</p>
-            ) : null}
+  return (
+    <div className="intFamilies">
+      <div className="icones">
+        <div className="module" id="moteur">
+          <div id="imgIcone">
+            <p>{ok}</p>
+            <button
+              onClick={() =>
+                setOk(ok === "Tout est parfait !" ? "" : "Tout est parfait !")
+              }
+            >
+              Moteur
+            </button>
           </div>
+        </div>
 
-          <div className="module" id="freins">
-            <div id="imgIcone">
-              <h1>Freins</h1>
-            </div>
+        <div className="module" id="freins">
+          <div id="imgIcone">
+            <h1>Freins</h1>
           </div>
+        </div>
 
-          <div className="module" id="chassis">
-            <div id="imgIcone">
-              <h1>Chassis</h1>
-            </div>
+        <div className="module" id="chassis">
+          <div id="imgIcone">
+            <h1>Chassis</h1>
           </div>
+        </div>
 
-          <div className="module" id="pneus">
-            <div id="imgIcone">
-              <h1>Pneus</h1>
-            </div>
+        <div className="module" id="pneus">
+          <div id="imgIcone">
+            <h1>Pneus</h1>
           </div>
+        </div>
 
-          <div className="module" id="carrosserie">
-            <div id="imgIcone">
-              <h1>Carrosserie</h1>
-            </div>
+        <div className="module" id="carrosserie">
+          <div id="imgIcone">
+            <h1>Carrosserie</h1>
           </div>
+        </div>
 
-          <div className="module" id="electricite">
-            <div id="imgIcone">
-              <h1>Electricite</h1>
-            </div>
+        <div className="module" id="electricite">
+          <div id="imgIcone">
+            <h1>Electricite</h1>
           </div>
+        </div>
 
-          <div className="module" id="echeances">
-            <div id="imgIcone">
-              <h1>Echeances</h1>
-            </div>
+        <div className="module" id="echeances">
+          <div id="imgIcone">
+            <h1>Echeances</h1>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ListeCar;
