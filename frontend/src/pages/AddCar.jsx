03979ab6ec18357836e7fc435660dc8a4dd9f32e
LaurentMahieu <mahieu.l@hotmail.fr>
2019-12-10 17:44:07 +0100

    Dynamique  country letter of the plate registration

--- frontend/src/pages/AddCar.jsx
+++ frontend/src/pages/AddCar.jsx
@@ -9,25 +9,29 @@ export default function AddCarr() {
     <div id="addCar">
       <header>
         <button className="close">X</button>
-        <p class="h1">IDENTIFICATION</p>
+        <p className="h1">IDENTIFICATION</p>
       </header>
       <div id="vehicleRegistration">
         <p className="h2">IMMATRICULATION</p>
         <div id="plate">
-          <div id="country">GB{country}</div>
+          <div id="country">{country}</div>
           <input
             id="registration"
             name="registration"
             type="text"
             value={registration}
             placeholder=" _ _ - _ _ _ - _ _"
-            onChange={evt => setRegistration(evt.target.value)}
+            onChange={evt => (setRegistration = evt.target.value)}
           ></input>
         </div>
-        <select name="country" id="coutry-select">
+        <select
+          name="country"
+          id="coutry-select"
+          onChange={evt => setCountry(evt.target.value)}
+        >
+          <option>choisir la nationalité</option>
           <option value="F">France</option>
           <option value="B">Belgique</option>
-          <option value="D">Deutschland</option>{" "}
           <option value="GB">Great Britain</option>
           <option value="S">Spain</option>
         </select>
