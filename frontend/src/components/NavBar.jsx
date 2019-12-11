5e69000e42b46833db5141990c1b16addc348d6c
Roche Pauline <rochepauline6@gmail.com>
2019-12-11 10:17:12 +0100

    renaming new icons and integrating in navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,12 +10,12 @@ function NavBar() {
       <ul>
         <li>
           <Link to="/">
-            <img src="/pictures/ico_perso.png" alt="" />
+            <img src="/pictures/icons/profile.png" alt="" />
           </Link>
         </li>
         <li>
           <Link to="/">
-            <img src="/pictures/ico_perso.png" alt="" />
+            <img src="/pictures/icons/dashboard.png" alt="" />
           </Link>
         </li>
         <li id="kmUpdate">
@@ -25,17 +25,17 @@ function NavBar() {
               showModalKm(true);
             }}
           >
-            KM
+            <img src="/pictures/icons/km_update.png" alt="" />
           </button>
         </li>
         <li>
           <Link to="/">
-            <img src="/pictures/ico_perso.png" alt="" />
+            <img src="/pictures/icons/tool.png" alt="" />
           </Link>
         </li>
         <li>
           <Link to="/">
-            <img src="/pictures/ico_perso.png" alt="" />
+            <img src="/pictures/icons/sell.png" alt="" />
           </Link>
         </li>
       </ul>
