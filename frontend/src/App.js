f71722ecef619c5f112801924496a812aa753aca
LaurentMahieu <mahieu.l@hotmail.fr>
2019-12-12 17:45:45 +0100

    clean app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import ModalConfirmInfos from "./components/ModalConfirmInfos";
-import ListCar from "./components/ListCar";
 import Identification from "./pages/Identification";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -13,16 +11,14 @@ import AddCar from "./pages/AddCar";
 function App() {
   return (
     <div className="App">
-      <ListCar />
       <Switch>
         <Route path="/identification" component={Identification} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/addCar" component={AddCar}
+        <Route path="/addCar" component={AddCar} />
+        <Route path="/home" component={HomePage} />
       </Switch>
-      <HomePage />
       <NavBar />
-      <ModalConfirmInfos />
     </div>
   );
 }
