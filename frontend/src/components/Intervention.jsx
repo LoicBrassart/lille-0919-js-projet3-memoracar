12f19f1cbc4be116084ba7adcdc6b4321707cc33
Blandine <blandine.mallart@gmail.com>
2019-12-17 16:09:50 +0100

    component intervention + SCSS

--- frontend/src/components/Intervention.jsx
+++ frontend/src/components/Intervention.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-import "./style/Historic.scss";
+import "./style/Intervention.scss";
 import InterventionCard from "./InterventionCard";
+import { useParams } from "react-router-dom";
 
 function Intervention(props) {
+  const { family } = useParams();
   return (
-    <div className="HistoricBox">
-      <div className="module">
-        <div id="imgIcone">
-          <img src="/pictures/icons/blue_motor.png" id="motor" alt="/" />
-          <h1>Moteur</h1>
-          <p>
-            Le kilométrage actuel du véhicule ainsi que l'historique enregistré
-            indiquent que vos systèmes sont à jour.
-          </p>
-        </div>
+    <div className="InterventionBox">
+      <div className="family">
+        <img src={`/pictures/icons/blue_${family}.png`} id="motor" alt="/" />
+        <h1>{family}</h1>
+        <p>
+          Le kilométrage actuel du véhicule ainsi que l'historique enregistré
+          indiquent que vos systèmes sont à jour.
+        </p>
       </div>
-      <div>
+      <div className="notification">
         <div className="ToCome">
           {props.Motor.Futur.map((item, i) => {
             return <InterventionCard item={item} key={i} />;
