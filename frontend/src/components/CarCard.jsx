7b874492832df129ce008d91ed6d5ff95252cfe1
Blandine <blandine.mallart@gmail.com>
2020-01-03 17:13:51 +0100

    visualisation front API

--- frontend/src/components/CarCard.jsx
+++ frontend/src/components/CarCard.jsx
@@ -5,12 +5,10 @@ import { Link } from "react-router-dom";
 
 export default function CarCard() {
   const [user, setUser] = useState([]);
-  const [date, setDate] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:5000/user/1/vehicle").then(({ data }) => {
+    axios.get("http://localhost:5000/user/3/vehicle").then(({ data }) => {
       setUser(data[0]);
-      setDate(data[0].date_mileage.slice(0, 10));
     });
   }, []);
 
@@ -18,14 +16,14 @@ export default function CarCard() {
     <div className="car">
       <img src="/pictures/ford.png" alt="ford"></img>
       <div className="info">
-        <h2>Dernier scan le :{date}</h2>
+        <h2>Dernier scan le :</h2>
         <h3>Kilométrage : {user.current_mileage} km</h3>
         <h1>
-          {user.brand} | {user.model}
+          {user.Marque} | {user.Modele}
         </h1>
         <h3>
-          {user.motorisation} ({user.horse_power} CH)
-          {user.production_year}
+          {user.Motorisation} ({user.Puissance} CH)
+          {user.Annee}
         </h3>
         <div className="choice">
           <Link to="/historic">
