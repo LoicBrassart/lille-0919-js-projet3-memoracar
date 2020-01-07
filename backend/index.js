9c68adb0f42582de25e21756d2183038df05e757
LaurentMahieu <mahieu.l@hotmail.fr>
2020-01-07 17:17:39 +0100

    export user in router

--- backend/index.js
+++ backend/index.js
@@ -14,49 +14,8 @@ app.use(passport.initialize());
 
 /* -------------------------------------------------------------- Public Routes */
 
-app.use("/auth", require("./auth"));
-
-// route d'identifiant avec l'ID
-app.get("/user/:id", (req, res) => {
-  const id = req.params.id;
-  connection.query(
-    `SELECT password, mail
-  FROM USER 
-  WHERE id=?`,
-    id,
-    (err, results) => {
-      if (err) {
-        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-        res.status(500).send("Error in user");
-      } else {
-        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-        res.json(results);
-      }
-    }
-  );
-});
-// route du véhicule de l'identifiant
-app.get("/user/:id/vehicle", (req, res) => {
-  const id = req.params.id;
-  // connection à la base de données, et sélection des vehicules
-  connection.query(
-    `SELECT vehicle.plate, brand, model, motorisation, horse_power, current_mileage, created_at, production_year, date_mileage
-    FROM 
-      vehicle 
-      JOIN users_has_vehicules ON vehicle.id=users_has_vehicules.id_vehicle 
-    WHERE id_user=?`,
-    id,
-    (err, results) => {
-      if (err) {
-        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-        res.status(500).send("Error in vehicles of user");
-      } else {
-        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-        res.json(results);
-      }
-    }
-  );
-});
+app.use("/auth", require("./route/auth"));
+app.use("/user", require("./route/user"));
 
 app.listen(port, err => {
   if (err) {
---
+++ backend/route/user.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { connection } = require("./conf");
+const router = express.Router();
+require("./passport-strategies");
+
+// route d'identifiant avec l'ID
+router.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+  connection.query(
+    `SELECT password, mail
+    FROM USER 
+    WHERE id=?`,
+    id,
+    (err, results) => {
+      if (err) {
+        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+        res.status(500).send("Error in user");
+      } else {
+        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+        res.json(results);
+      }
+    }
+  );
+});
+// route du véhicule de l'identifiant
+router.get("/user/:id/vehicle", (req, res) => {
+  const id = req.params.id;
+  // connection à la base de données, et sélection des vehicules
+  connection.query(
+    `SELECT vehicle.plate, brand, model, motorisation, horse_power, current_mileage, created_at, production_year, date_mileage
+      FROM 
+        vehicle 
+        JOIN users_has_vehicules ON vehicle.id=users_has_vehicules.id_vehicle 
+      WHERE id_user=?`,
+    id,
+    (err, results) => {
+      if (err) {
+        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+        res.status(500).send("Error in vehicles of user");
+      } else {
+        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+        res.json(results);
+      }
+    }
+  );
+});
+
+module.exports = router;
