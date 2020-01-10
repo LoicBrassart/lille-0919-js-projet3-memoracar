317a8e45052b9ae0f7f18cf091fe5f27fc5a9315
LaurentMahieu <mahieu.l@hotmail.fr>
2020-01-10 16:22:15 +0100

    add router get

--- backend/route/auth.js
+++ backend/route/auth.js
@@ -56,4 +56,12 @@ router.post("/login", (req, res) => {
   )(req, res);
 });
 
+router.get(
+  "/testAuth",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.status(200).send(`Welcome ${req.user.pseudo}, you're logged in !`);
+  }
+);
+
 module.exports = router;
