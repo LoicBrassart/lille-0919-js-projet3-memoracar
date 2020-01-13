1c38d5beeea2b8f1a3668f1a3d2d808bf9064036
Roche Pauline <rochepauline6@gmail.com>
2020-01-13 09:53:24 +0100

    deleting useless react import

--- frontend/src/components/KmUpdate.jsx
+++ frontend/src/components/KmUpdate.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import "./style/KmUpdate.scss";
