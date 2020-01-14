07d8d4ce1c7b5b0ae38a33fd991648c15e9007d4
Blandine <blandine.mallart@gmail.com>
2020-01-14 09:29:05 +0100

    nextmaintenance route

--- frontend/src/components/Intervention.jsx
+++ frontend/src/components/Intervention.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import "./style/Intervention.scss";
 import InterventionCard from "./InterventionCard";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Intervention(props) {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/vehicule/1/nextmaintenance")
+      .then(({ data }) => {
+        setUser(
+          data.filter(vehicule => {
+            return vehicule.famille === family;
+          })
+        );
+      });
+  }, []);
   const { family } = useParams();
+
   return (
     <div className="InterventionBox">
+      {/* <h1>{user[0] && user[0].prochaineEcheance}</h1> */}
       <div className="family">
         <img src={`/pictures/icons/blue_${family}.png`} id="motor" alt="/" />
         <h1>{family}</h1>
@@ -18,11 +34,11 @@ function Intervention(props) {
       </div>
       <div className="notification">
         <div className="ToCome">
-          {props.Motor.Futur.map((item, i) => {
+          {user.map((item, i) => {
             return <InterventionCard item={item} key={i} />;
           })}
         </div>
-        <div className="Present BoxEvent">{Date()}</div>
+        <div className="Present BoxEvent">{Date().slice(0, 15)}</div>
         <div className="Passed">
           {props.Motor.Passed.map((item, i) => {
             return <InterventionCard item={item} key={i} />;
