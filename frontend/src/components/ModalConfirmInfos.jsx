13fe0b7c5c7e978b88c59d59fc014cc99a7513a8
Roche Pauline <rochepauline6@gmail.com>
2020-01-15 17:49:24 +0100

    adding axios request to send new km and date to DB

--- frontend/src/components/ModalConfirmInfos.jsx
+++ frontend/src/components/ModalConfirmInfos.jsx
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import ReactModal from "react-modal";
 import "./style/ModalConfirmInfos.scss";
 import { useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+const { apiSite } = require("../conf");
 
 export default function ModalConfirmInfos(props) {
   const [showModal, setShowModal] = useState(false);
   const history = useHistory();
   const dispatch = useDispatch();
+  const kmToUpdate = useSelector(state => state.kmToUpdate);
+  const idVehicle = useSelector(state => state.user.carData.id);
+
   let date = new Date();
   let Month = "";
   if (date.getMonth() + 1 < 10) {
     Month = "0" + parseInt(date.getMonth() + 1);
   }
   const today = `${date.getDate()}-${Month}-${date.getFullYear()}`;
+  const todayEng = `${date.getFullYear()}-${Month}-${date.getDate()}`;
+
+  const sendingUpdate = (id, km, date) => {
+    axios.put(`${apiSite}/vehicule/${id}`, { km: km, date: date }).then();
+  };
 
   return (
     <div id="ModalConfirmInfos">
@@ -38,6 +48,7 @@ export default function ModalConfirmInfos(props) {
             className="modalButton"
             onClick={() => {
               setShowModal(false);
+              sendingUpdate(idVehicle, kmToUpdate.join(""), todayEng);
               history.push("/");
               dispatch({ type: props.type, value: today });
             }}
