8c63fff9b3868b720cd1ba6b2c67b594f7f189ed
LaurentMahieu <mahieu.l@hotmail.fr>
2020-01-16 15:59:25 +0100

    init restriction to submit form

--- frontend/src/pages/SelectCar.jsx
+++ frontend/src/pages/SelectCar.jsx
@@ -9,18 +9,54 @@ export default function SelectCar() {
   const [kilometrage, setKilometrage] = useState("");
   const [modele, setModele] = useState({});
   const [modeles, initModeles] = useState([]);
+  const year = new Date().getFullYear();
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/modelevehicule/marque")
       .then(({ data }) => {
-        console.log(data);
         initModeles(data);
+        limitStringLength();
       });
   }, []);
 
+  function addCar(e) {
+    e.preventDefault();
+    if (immatriculation === "") {
+      alert(`immat prob`);
+    } else if (vin.length !== 17) {
+      console.log(`vin incomplet`);
+    } else if (
+      parseInt(kilometrage) >= Math.pow(10, 6) ||
+      isNaN(kilometrage) ||
+      kilometrage === ""
+    ) {
+      console.log("kilométrage incorrect");
+    } else if (
+      parseInt(année) < 1900 ||
+      parseInt(année) > year ||
+      année === "" ||
+      isNaN(année)
+    ) {
+      console.log(`car crash`);
+    } else {
+      console.log(`car added`);
+    }
+  }
+
+  function limitStringLength() {
+    return (
+      document.getElementById("année").maxLength,
+      document.getElementById("kilometrage").maxLength,
+      document.getElementById("VIN").maxLength
+    );
+  }
   return (
-    <form method="post">
+    <form
+      onSubmit={e => {
+        addCar(e);
+      }}
+    >
       <div id="selectCar">
         <header>
           <h1>Ajouter un véhicule</h1>
@@ -68,26 +104,29 @@ export default function SelectCar() {
             value={vin}
             placeholder="_ _ _ - _ _ _ _ _ _ - _ _ _ _ _ _ _ _"
             onChange={evt => setVin(evt.target.value)}
+            maxLength="17"
           ></input>
 
           <h2>Kilométrage</h2>
           <input
             id="kilometrage"
             name="kilometrage"
-            type="number"
+            type="text"
             value={kilometrage}
             placeholder="_ _ _ _ _ _"
             onChange={evt => setKilometrage(evt.target.value)}
+            maxLength="7"
           ></input>
 
           <h2>Année</h2>
           <input
             id="année"
             name="année"
-            type="number"
+            type="text"
             value={année}
             placeholder="_ _ _ _"
             onChange={evt => setAnnée(evt.target.value)}
+            maxLength="4"
           ></input>
         </div>
         <button className="button" type="submit">
