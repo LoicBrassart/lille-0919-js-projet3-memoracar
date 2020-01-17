89418e5c8c47af00f0a3bf0378bc179b72fd28bc
LaurentMahieu <mahieu.l@hotmail.fr>
2020-01-17 17:55:56 +0100

    delete useless import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useReducer } from "react";
+import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./App.css";
