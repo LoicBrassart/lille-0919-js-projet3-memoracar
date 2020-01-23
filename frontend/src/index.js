42476d728831f8a768f7e31fcdb37a2e196d426a
dderancourt <55059091+dderancourt@users.noreply.github.com>
2020-01-23 09:42:50 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducer from "./reducer/reducer";
 import "./reset.css";
