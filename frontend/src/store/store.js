6c4b7e7b2f45ed7ead28c10408e101b87a94b92c
Blandine <blandine.mallart@gmail.com>
2020-01-23 09:57:35 +0100

    clean store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,83 +1,9 @@
 const initialState = {
-  ToCome: [
-    {
-      date: "01 / 02 / 2020",
-      title: "remplacement des pneus",
-      icon: "/pictures/icons/blue_pneus.png",
-      place: "NORAUTO Seclin"
-    },
-    {
-      date: "01 / 03 / 2020",
-      title: "remplacement des pneus",
-      icon: "/pictures/icons/blue_pneus.png",
-      place: "NORAUTO Seclin"
-    },
-    {
-      date: "01 / 04 / 2020",
-      title: "remplacement des pneus",
-      icon: "/pictures/icons/blue_pneus.png",
-      place: "NORAUTO Seclin"
-    }
-  ],
-  Passed: [
-    {
-      date: "01 / 02 / 2010",
-      title: "remplacement des pneus",
-      icon: "/pictures/icons/blue_pneus.png",
-      place: "NORAUTO Seclin"
-    },
-    {
-      date: "01 / 03 / 2010",
-      title: "remplacement des pneus",
-      icon: "/pictures/icons/blue_pneus.png",
-      place: "NORAUTO Seclin"
-    },
-    {
-      date: "01 / 04 / 2010",
-      title: "remplacement des pneus",
-      icon: "/pictures/icons/blue_pneus.png",
-      place: "NORAUTO Seclin"
-    }
-  ],
+  ToCome: [],
+  Passed: [],
   kmToUpdate: ["", "", "", "", "", ""],
   numOfKmUpdates: 0,
   isMileageCorrect: true,
-  Motor: {
-    Futur: [
-      {
-        title: "huile",
-        subtitle: "Vidange recommandée dans: ",
-        icon: "/pictures/icons/blue_oil.png",
-        km: "9948 km"
-      }
-    ],
-    Passed: [
-      {
-        title: "huile",
-        subtitle: "Vidange effectuée à: ",
-        icon: "/pictures/icons/blue_oil.png",
-        km: "26 450 km"
-      },
-      {
-        title: "huile",
-        subtitle: "Vidange effectuée à: ",
-        icon: "/pictures/icons/blue_oil.png",
-        km: "20 450 km"
-      },
-      {
-        title: "huile",
-        subtitle: "Vidange effectuée à: ",
-        icon: "/pictures/icons/blue_oil.png",
-        km: "450 km"
-      },
-      {
-        title: "huile",
-        subtitle: "Vidange effectuée à: ",
-        icon: "/pictures/icons/blue_oil.png",
-        km: "26 450 km"
-      }
-    ]
-  },
   user: {
     id: -1,
     mail: "",
