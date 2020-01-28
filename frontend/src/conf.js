5008329d39de9342ecba86fcb4e965eb2b5a22b6
Blandine <blandine.mallart@gmail.com>
2020-01-28 17:09:08 +0100

    conf img

--- frontend/src/conf.js
+++ frontend/src/conf.js
@@ -3,4 +3,6 @@ require("dotenv").config();
 let CONFIG = {};
 CONFIG.siteTitle = process.env.REACT_APP_SITE_TITLE;
 CONFIG.apiSite = process.env.REACT_APP_LOCALHOST;
+CONFIG.img =
+  process.env.REACT_APP_IMG_LOGO || `/pictures/logos/shinyHedgehog.jpg`;
 module.exports = CONFIG;
